6e640c5f5c2251d657b8491b6192c5986d874664
Rishabh Malik <rishabhmalik249@gmail.com>
2018-02-04 09:48:58 +0530

    updated

--- mch.sol
+++ mch.sol
@@ -4,23 +4,29 @@ contract delivery{
     address [] public companies;
     address [] public deliveryPerson;
     uint256 [] public productsId;
-    mapping (address=>uint256) public bids;
+    mapping (uint256=>address) public IDpeBanda;
+    mapping (address=>uint256) public BandeKePaise;
     mapping (address=>uint256) public balances;
     mapping (uint256=>string) public From;
     mapping (uint256=>string) public To;
     
+    event status(string msgs);
+    
     function joinCompanyCommunity () public {
         companies.push(msg.sender);
+        status("Joined Company community");
     }
     
     function joinAsDeliveryPerson () payable public {
         deliveryPerson.push(msg.sender);
         balances[msg.sender]+=msg.value;
+        status("Joined delivers community");
     }
     
     function validDeliveryPerson(address add) private returns (bool){
         for(uint i=0; i < deliveryPerson.length; i++){
             if(deliveryPerson[i]==add){
+                status("valid delivery person");
                 return true;
             }
         }
@@ -30,6 +36,7 @@ contract delivery{
      function validCompany(address add) private returns (bool){
         for(uint i=0; i < companies.length; i++){
             if(companies[i]==add){
+                status("valid company");
                 return true;
             }
         }
@@ -41,15 +48,27 @@ contract delivery{
             productsId.push(productid);
             From[productid]=from;
             To[productid]=to;
+            status("Product added");
         }
     }
     
-    function bidForProduct(uint256 money)public{
+    function getProducts() view public returns(uint256[]){
+        return productsId;
+    }
+    
+    function bidForProduct(uint256 money,uint256 productid)public{
         if(validDeliveryPerson(msg.sender)==true){
-            
+            IDpeBanda[productid] = msg.sender;
+            BandeKePaise[msg.sender] = money;
+            status("bid added");
         }
     }
     
+    function getBids(uint256 productid) view public returns(uint256) {
+        return BandeKePaise[IDpeBanda[productid]];
+    }
     
-    
+    function getWinner(uint256 productid) view public returns(address) {
+        return IDpeBanda[productid];
+    }
 }
